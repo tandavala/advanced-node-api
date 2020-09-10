f535cb13626d80ea84bf932c1ece26e157b5583a
tandavala <jose.tandavala@gmail.com>
2020-09-10 20:06:02 +0100

    adding env variables

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,10 @@
 module.exports = {
-  googleClientID:
-    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
-  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
-  mongoURI: "mongodb://localhost/blog_dev",
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379",
-  accessKeyId: "AKIAIWSXFHRM7F6Z3NWQ",
-  secretAccessKey: "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",
-  secretOrKey: "amendicasikulo",
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  mongoURI: process.env.mongoURI,
+  cookieKey: process.env.cookieKey,
+  redisUrl: process.env.redis,
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  secretOrKey: process.env.secretOrKey,
 };
