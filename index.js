fb23d85c6abc5f3b9b97c0ce29485455d63c2181
tandavala <jose.tandavala@gmail.com>
2020-09-10 19:04:59 +0100

    adding the ability to create a user

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('ola mundo agora...')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
+
+require("./services/cache");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello Advanced api"));
+
+require("./routes/userRoute")(app);
+
+if (["production", "ci"].includes(process.env.NODE_ENV)) {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,36 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../config/keys");
+
 const User = require("../models/User");
 
 module.exports = (app) => {
-  app.post("/", (req, res) => {
-    res.send("create User");
+  app.post("/users", (req, res) => {
+    const { name, password } = req.body;
+
+    if (!name && !password) {
+      return res.status(400).json({ msg: "name ou password required!" });
+    }
+    User.findOne({ name: name }).then((user) => {
+      if (user) {
+        return res.status(400).json({ msg: "This name is already in use" });
+      } else {
+        const newUser = new User({
+          name: name,
+          password: password,
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    });
   });
 };
